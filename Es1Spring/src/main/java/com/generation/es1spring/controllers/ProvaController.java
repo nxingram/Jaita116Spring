1034f5f75d496e53fc38248195c7476022483467
nxingram <nxingram@users.noreply.github.com>
2024-05-08 18:33:02 +0200

    update commenti

--- Es1Spring/src/main/java/com/generation/es1spring/controllers/ProvaController.java
+++ Es1Spring/src/main/java/com/generation/es1spring/controllers/ProvaController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.es1spring.model.PersonaDTO;
 
-/*
+
+/**
  * Corso java Bogliaccino 
  * https://github.com/maboglia/CorsoJava/tree/master/appunti
  * 
@@ -21,18 +22,19 @@ import com.generation.es1spring.model.PersonaDTO;
  * https://github.com/maboglia/Fondamenti/blob/master/012_REST.md
  * 
  * Spring-Boot-Tutorial-base
- * https://github.com/nxingram/spring-boot-java/tree/6e10d34edb986e899b54ba73beca9b862f3ebd79/Spring-Boot-Tutorial-base
+ * https://github.com/nxingram/spring-boot-java/tree/main/Spring-Boot-Tutorial-base
  */
-
-
-
 @RestController // controller di tipo REST
 @RequestMapping("/api/json") // rotta-action rest api
 public class ProvaController {
 //RestController rispondere alle ichieste proventi da applicazioni o browser
 	
-	// finisco qui quando chiamo l'url "localhost:8080/api/json" con metodo "Get"
-	//produces: specifica il tipo di dato restituito nelle Response
+	/**
+	 * finisco qui quando chiamo l'url:
+	 * "localhost:8080/api/json" con metodo "Get"
+	 * produces: specifica il tipo di dato restituito nelle Response
+	 * @return PersonaDTO
+	 */
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) //="application/json"
 	public PersonaDTO getPersona() {
 		PersonaDTO persona = new PersonaDTO();
@@ -44,16 +46,24 @@ public class ProvaController {
 	}
 	
 	
+	/**
+	 * localhost:8080/api/json/persona
+	 * @param persona
+	 * @return PersonaDTO
+	 */
 	//@RequestMapping(method = RequestMethod.POST)
 	@PostMapping("/persona") //abbreviazione di RequestMapping
-	// localhost:8080/api/json/persona
 	public PersonaDTO postPersona(@RequestBody PersonaDTO persona) {	
 		persona.setNome(persona.getNome().toUpperCase());
 		persona.setCognome(persona.getCognome().toUpperCase());
 		return persona;
 	}
 	
-	// localhost:8080/api/json/persona/4
+	/**
+	 * localhost:8080/api/json/persona/4
+	 * @param id_variabile numero intero
+	 * @return numero intero
+	 */
 	@GetMapping("/persona/{id_segnaposto}")
 	public int getPersonaById(@PathVariable("id_segnaposto") int id_variabile)
 	{
